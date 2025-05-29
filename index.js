459a79166dd0f661246bf58a98181f3c04d23586
Sophearum Siyonn <143072076+SophearumSiyonn@users.noreply.github.com>
2025-05-29 15:57:25 +0700

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+//use proxy serve rto redirect the incoming request
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createProxyServer({});
+
+//redirect to the student micro-service
+app.use('/student', (req, res) => {
+    console.log("INSIDE API GATEWAY STUDENT ROUTE")
+    proxy.web(req,res, {target:'http://localhost:5005'});
+})
+
+//redirect to the teacher micro-service
+app.use('/teacher', (req, res) => {
+    console.log("INSIDE API GATEWAY TEACHER ROUTE")
+    proxy.web(req,res, {target:'http://localhost:5010'});
+})
+// START THE EXPRESS SERVER. 5000 is the PORT NUMBER
+app.listen(4008, () => {
+    console.log('EXPRESS Server Started at Port No: 4008')})
+
+
