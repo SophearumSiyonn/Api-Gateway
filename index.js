3ab03f939689dd45659e5a464818d9effc51cde3
Sophearum Siyonn <143072076+SophearumSiyonn@users.noreply.github.com>
2025-05-30 16:19:03 +0700

    Update

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer({});
 //redirect to the student micro-service
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req,res, {target:'172.31.93.127'});
+    proxy.web(req,res, {target:'172.31.93.127:5003'});
 })
 
 //redirect to the teacher micro-service
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req,res, {target:'172.31.87.96'});
+    proxy.web(req,res, {target:'172.31.87.96:5008'});
 })
 // START THE EXPRESS SERVER. 5000 is the PORT NUMBER
 app.listen(4008, () => {
