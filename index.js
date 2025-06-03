c23ef361c12a9a04ccada67c60dac3eff5f3ff95
Sophearum Siyonn <143072076+SophearumSiyonn@users.noreply.github.com>
2025-06-03 15:43:53 +0700

    Update

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 const express = require('express');
-const app = express();
+const app = express()
 
-//use proxy serve rto redirect the incoming request
+//USE PROXY SERVER TO REDIRECT THE INCOMMING REQUEST
 const httpProxy = require('http-proxy')
-const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer();
 
-//redirect to the student micro-service
-app.use('/student', (req, res) => {
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const JWT_SECRETE = process.env.JWT_SECRETE;
+
+function authToken(req, res, next) {
+    console.log(req.headers.authorization)
+    const header = req?.headers.authorization;
+    const token = header && header.split(' ')[1];
+
+    if (token == null) return res.status(401).json("Please send token");
+
+    jwt.verify(token, JWT_SECRETE, (err, user) => {
+        if (err) return res.status(403).json("Invalid token", err);
+        req.user = user;
+        next()
+    })
+}
+
+function authRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json("Unauthorized");
+        }
+        next();
+    }
+}
+
+//REDIRECT TO THE STUDENT MICROSERVICE
+app.use('/student',authToken, authRole('student'), (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req,res, {target:'18.205.188.62:5005'});
+    proxy.web(req, res, { target: 'http://localhost:5008' });
 })
 
-//redirect to the teacher micro-service
-app.use('/teacher', (req, res) => {
+//REDIRECT TO THE TEACHER MICROSERVICE
+app.use('/teacher', authToken, authRole('teacher'),(req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req,res, {target:'44.212.62.163:5010'});
+    proxy.web(req, res, { target: 'http://localhost:5001' });
 })
-// START THE EXPRESS SERVER. 5000 is the PORT NUMBER
-app.listen(4008, () => {
-    console.log('EXPRESS Server Started at Port No: 4008')})
 
+//REDIRECT TO THE LOGIN(Authentication) MICROSERVICE
+app.use('/auth', (req, res) => {
+    proxy.web(req, res, { target: 'http://localhost:5002' });
+})
 
+app.listen(4000, () => {
+    console.log("API Gateway Service is running on PORT NO : 4000")
+})
