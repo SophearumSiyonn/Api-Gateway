80eef03fcf3531a68c2cce876f2726d861716cd3
Sophearum Siyonn <143072076+SophearumSiyonn@users.noreply.github.com>
2025-06-03 15:55:23 +0700

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const proxy = httpProxy.createProxyServer();
 
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const JWT_SECRETE = process.env.JWT_SECRETE;
+const JWT_SECRETE = process.env.JWT_SECRET;
 
 function authToken(req, res, next) {
     console.log(req.headers.authorization)
